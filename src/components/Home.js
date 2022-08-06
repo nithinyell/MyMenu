33922dff3b353395ddfb45ee259b2d4b83e49d1e
Nithin Yell <nithinyell@gmail.com>
2022-08-07 04:41:09 +0530

    update 4

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import {addDoc, collection, onSnapshot} from 'firebase/firestore'
 import {projectFirestore} from '../firebase/Config'
 import Promotions from "./Promotions";
+import save from '../images/save.png'
+import delet from '../images/delet.png'
+import logo from '../images/D4Logo.png'
 
 function Home() {
     const [menu, setMenu] = useState([]);
@@ -11,13 +14,14 @@ function Home() {
     const menuCollectionRef = collection(projectFirestore, "menu")
 
     useEffect(() => {
-      const getUsers = async () => {
+      fetchItems()
+    })
+
+    const fetchItems = async() => {
         onSnapshot(menuCollectionRef, (snaps) => {
             setMenu(snaps.docs.map((doc) => ({...doc.data(), id: doc.id})))
         })
-      }
-      getUsers()
-    })
+    }
 
     const createItem = async() => {
         if (title.length > 0 && price.length > 0) {
@@ -25,23 +29,41 @@ function Home() {
         }
     }
 
-    return (<div className="Home">
-        <div>
-            <h1 className="font-link">Arya - Order Your Favourite Food</h1>
-            <Promotions />
+    const deleteItem = async(id) => {
+        console.log("delete", id)
+    }
+
+    const updateitem = async(id, title, price) => {
+
+    }
+
+    return (<div style={{ padding: 20 }}>
+        <div style={{}}>
+            <div>
+                <div className="App-header">
+                    <h1 className="font-link">Order Your Favourite Food</h1>
+                    <img src={logo} alt="Logo" style={{ height: 100, width: 100}} />
+                </div>
+            </div>
+            <div>
+                <Promotions />
+            </div>
         </div>
-        <div>
+        <div style={{}}>
             <input placeholder="Title" onChange={(event) => { setTitle(event.target.value) }}></input>
             <input placeholder="Price" onChange={(event) => { setPrice(event.target.value) }}></input>
-            <button onClick={createItem}>Submit</button>
-        </div>    
+            <button><img src={save} style={{ height: 25, width: 25, padding: 0 }} alt="" onClick={createItem} /></button>
+        </div>
         <div>
             {menu.map((m) => {
                 return (
-                    <div className="menu-page" key={m.id}>
-                        <h1 className="font-link">
+                    <div className="" key={m.id}>
+                        <h3 className="font-link menu-items-fontSize">
                             {m.title} - ${m.price}
-                        </h1>
+                        </h3>
+                        <div>
+                            <button src={delet} style={{ height: 25, width: 25, padding: 0 }} alt="" onClick={() => { deleteItem(m.id) }}></button>
+                        </div>
                     </div>
                 )
             })}
--- src/components/Promotions.js
+++ src/components/Promotions.js
@@ -2,12 +2,12 @@ import Carousel from 'carousel-react-rcdev'
 
 function Promotions() {
     return (
-        <Carousel max_width={0}>
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQc6hKsCbd8aSL0RXIkzA0DVjb3ThlvkSVNQ&usqp=CAU' alt='imagem' title='Breakfast' />
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-Tcixgt0wQ1J8svx8zlXKSAnmQaS1mD2D6w&usqp=CAU' alt='imagem' title='Lunch' />
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnGhYO_YBEME84rZeDNTeSV-i0XOU-Qs4Ovw&usqp=CAU' alt='imagem' title='Snacks' />
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8yTIjWX8FBa4dd2XbL8Px7JjGVNa2Xk4R9Q&usqp=CAU' alt='imagem' title='Snacks' />
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNG5zh3c_MkvUrLb5bAX_7u_AvWQHCF2AtZw&usqp=CAU' alt='imagem' title='Snacks' />
+        <Carousel max_width={window.innerWidth}>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQc6hKsCbd8aSL0RXIkzA0DVjb3ThlvkSVNQ&usqp=CAU' alt='imagem' title='' />
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-Tcixgt0wQ1J8svx8zlXKSAnmQaS1mD2D6w&usqp=CAU' alt='imagem' title='' />
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnGhYO_YBEME84rZeDNTeSV-i0XOU-Qs4Ovw&usqp=CAU' alt='imagem' title='' />
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8yTIjWX8FBa4dd2XbL8Px7JjGVNa2Xk4R9Q&usqp=CAU' alt='imagem' title='' />
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNG5zh3c_MkvUrLb5bAX_7u_AvWQHCF2AtZw&usqp=CAU' alt='imagem' title='' />
         </Carousel>
     )
 }
