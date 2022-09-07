e205696b4118a1957e651abff1fff93b447f1d81
Nithin Yell <nithinyell@gmail.com>
2022-09-07 19:19:06 +0530

    Update Config to new fire base

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -2,13 +2,13 @@ import {initializeApp} from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyABCl-Y-Ia-yWfE8LKZmLWu592BO2LPklk",
-  authDomain: "my-menu-59199.firebaseapp.com",
-  projectId: "my-menu-59199",
-  storageBucket: "my-menu-59199.appspot.com",
-  messagingSenderId: "447894307168",
-  appId: "1:447894307168:web:60d174d7b2c11888795e05",
-  measurementId: "G-YHCZML5QMF"
+  apiKey: "AIzaSyAiPVl5d-QFfwQFRIKBcSic9NN6IVxqBis",
+  authDomain: "my-menu-74e62.firebaseapp.com",
+  projectId: "my-menu-74e62",
+  storageBucket: "my-menu-74e62.appspot.com",
+  messagingSenderId: "499983731152",
+  appId: "1:499983731152:web:b5c529cc58aea96e0fd1d5",
+  measurementId: "G-PSNPJZQL83"
 };
 
 // Initialize Firebase
