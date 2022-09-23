0fdb0d04c9de482ab68463f99b0714250bde41ff
ernestrajkumard4 <84857636+ernestrajkumard4@users.noreply.github.com>
2022-09-23 14:05:07 +0530

    New page with only item display

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import React from "react";
+
+import Route from "./components/Route"
 import './App.css';
 import Home from './components/Home';
+import Menu from './components/Menu';
 
-function App() {
-  return (
-    <div className="My Menu">
-        <main className=''>
-          <Home/>
-        </main>
-    </div>
-  );
+export default () => {
+	return (
+		<div className="My Menu">
+			<Route path="/">
+				<Home />
+			</Route>
+			<Route path="/menu">
+				<Menu />
+			</Route>
+		</div>
+	)
 }
-
-export default App;
